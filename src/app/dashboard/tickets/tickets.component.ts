e6efada292b2e98c93dd0c46b2d054aa564e9d7c
emre1n <emre1n@protonmail.com>
2025-07-26 17:54:17 +0300

    add for loop to tickets component template

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+import type { Ticket } from './ticket.model';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
-import { Ticket } from './ticket.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
