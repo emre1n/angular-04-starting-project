daa21265ee064adc97ccb97f5ebb2118c0d56207
emre1n <emre1n@protonmail.com>
2025-07-26 17:39:41 +0300

    add ticket model with Ticket interface

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-tickets',
@@ -9,4 +10,6 @@ import { NewTicketComponent } from './new-ticket/new-ticket.component';
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+}
