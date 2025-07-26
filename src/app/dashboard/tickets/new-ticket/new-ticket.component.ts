1ab6ec90ed83649cd3b19f886cf0d38605c2133c
emre1n <emre1n@protonmail.com>
2025-07-26 15:42:23 +0300

    implement template variables approach to get title and text of support tickets in new-ticket component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -6,8 +7,13 @@ import { ControlComponent } from '../../../shared/control/control.component';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit(title: string, ticketText: string) {
+    console.log('ENTERED TITLE: ' + title);
+    console.log('ENTERED TICKET TEXT: ' + ticketText);
+  }
+}
