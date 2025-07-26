012e588c83d791e8af1f71c43169add65459341e
emre1n <emre1n@protonmail.com>
2025-07-26 17:33:49 +0300

    add signal effect function to track the change of current status in server-status component

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -2,8 +2,10 @@ import {
   AfterViewInit,
   Component,
   DestroyRef,
+  effect,
   inject,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -14,12 +16,18 @@ import {
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit, AfterViewInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // currentStatus: 'online' | 'offline' | 'unknown' = 'online'; // @NOTE: this is a regular property
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline'); // @NOTE: this is a signal
+
   // private interval?: ReturnType<typeof setInterval>;
 
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log('CURRENT STATUS: ', this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     console.log('ON INIT');
@@ -27,11 +35,11 @@ export class ServerStatusComponent implements OnInit, AfterViewInit {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
@@ -48,3 +56,22 @@ export class ServerStatusComponent implements OnInit, AfterViewInit {
   //   clearTimeout(this.interval); // @Note: to clean up the interval and avoid memory leaks
   // }
 }
+
+/*
+Signal Effects Cleanup Functions
+When working with Signal effects, you sometimes might need to perform some cleanup work before the effect function runs again (e.g., to clear some timer or something like that).
+
+Angular's effect() allows you to do that!
+
+It does provide you with an onCleanup hook which you can execute as part of your effect function to define what should happen before the effect code runs the next time:
+
+effect((onCleanup) => {
+  const tasks = getTasks();
+  const timer = setTimeout(() => {
+    console.log(`Current number of tasks: ${tasks().length}`);
+  }, 1000);
+  onCleanup(() => {
+    clearTimeout(timer);
+  });
+});
+*/
